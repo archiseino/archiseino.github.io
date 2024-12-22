b4d283c0f7aa966bab0af6718206d03b4f7a81db
Arsyadana Estu - archise1 <162825947+archiseino@users.noreply.github.com>
2024-12-22 18:32:18 +0000

    feat: adding watcher auto save

---
+++ _plugins/watcher-patch.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+# Auto Refreshment
+
+require 'jekyll-watch'
+
+module Jekyll
+  module Watcher
+    extend self
+
+    alias_method :original_listen_ignore_paths, :listen_ignore_paths
+
+    def listen_ignore_paths(options)
+        original_listen_ignore_paths(options) + [%r!.*\.TMP!i]
+    end
+  end
+end
